fbf81b878bea008797b443e32732a18a160a3472
robles <robles.com>
2019-03-24 22:18:19 +0800

    I changed my classmate's repo

--- application/views/index.php
+++ application/views/index.php
@@ -3,11 +3,13 @@
 <html>
 <head>
 	<title>Robles</title>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/style.css'); ?">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/style.css'); ?>">
 </head>
 <body>
 
 <p>GIT!</p>
+<p>I changed Roble's View</p>
+
 
 </body>
 </html>
