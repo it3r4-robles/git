c1717e893489de821db68acb8be551e711e72c38
robles <robles.com>
2019-03-24 21:48:12 +0800

    robles git sample

---
+++ application/views/index.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Robles</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/style.css'); ?">
+</head>
+<body>
+
+<p>GIT!</p>
+
+</body>
+</html>
